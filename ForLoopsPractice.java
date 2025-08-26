9c89e13530c89959359d01ff28a98b26885020fc
Notoriousrios <96099036+Notoriousrios@users.noreply.github.com>
2025-08-26 11:47:02 -0700

    Add files via upload

---
+++ ForLoopsPractice.java
@@ -0,0 +1,12 @@
+// rVE String Objects
+public class ForLoopsPractice {
+    public static void main(String [] args) {
+
+        // A loop must have a loop control variable
+        //The loop control variable must be initialized, checked and changed
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
